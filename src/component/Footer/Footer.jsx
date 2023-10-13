c6b6fbfe78aa81050ad91f1f73d557b645eaefd7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-13 23:25:56 +0600

    make responsive footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,19 @@
-
+import { Link } from "react-router-dom";
+import img1 from "../../assets/images/footer/facebook-48.png"
+import img2 from "../../assets/images/footer/linkedin-48.png"
+import img3 from "../../assets/images/footer/twitter-48.png"
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer section</h2>
+        <div className="p-32 bg-blue-950 text-white text-sm text-center">
+            <h2 className="p-6">@Dont Copy the Page</h2>
+            <hr />
+            <div className="flex justify-center items-center gap-2 p-8">
+                <Link to="/"><img className="w-10" src={img1} alt="" /></Link>
+                <Link to="/"><img className="w-10" src={img2} alt="" /></Link>
+                <Link to="/"><img className="w-10" src={img3} alt="" /></Link>
+                
+            </div>
         </div>
     );
 };
