ee666b152d27685a2135915fef17047bff45cbfe
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-13 23:11:35 +0600

    responsive cat section

--- src/component/Cat/Cat.jsx
+++ src/component/Cat/Cat.jsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 const Cat = (props) => {
     const {img,cat_name,cat_age,price}=props.cat;
+    const newLocal = "bg-fuchsia-700 px-6 py-2 rounded-md text-white mt-3 inline-block";
     return (
-        <div className="grid grid-cols-2 gap-20 mt-20 bg-blue-200 rounded-lg m-20 items-center">
-            <img className="w-3/5" src={img} alt="" />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 mt-20 bg-blue-200 rounded-md m-20 items-center p-10  lg:p-0">
+            <img className="w-full lg:w-3/5 rounded-md" src={img} alt="" />
             <div>
                 <h2>Cat Name :<span className="text-2xl font-bold"> {cat_name}</span> </h2>
                 <h2>Cat Age :{cat_age} </h2>
                 <h3>Price:<span className="text-xl font-bold"> {price}</span> </h3>
-                <Link to="/" className="bg-fuchsia-700 px-6 py-2 rounded-md text-white mt-3 inline-block" >Learn More</Link>
+                <Link to="/" className={newLocal} >Learn More</Link>
             </div>
         </div>
     );
